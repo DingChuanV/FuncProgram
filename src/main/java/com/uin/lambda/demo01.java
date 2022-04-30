c95e200102b1a994ef9b72e50e4043dbf72d0ae8
wanglufei <2686221966@qq.com>
2022-04-30 17:55:32 +0800

    初始化工程

--- src/main/java/com/uin/lambda/demo01.java
+++ src/main/java/com/uin/lambda/demo01.java
@@ -2,6 +2,12 @@ package com.uin.lambda;
 
 public class demo01 {
     public static void main(String[] args) {
-        
+        //我们在创建线程启动时可以使用匿名内部类的写法
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("你知道吗 我很爱五一假期");
+            }
+        }).start();
     }
 }
